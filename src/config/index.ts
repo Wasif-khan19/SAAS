0c8a6e17e8d5c60953ecaad1860cedf343078f49
Wasif <wk5599426@gmail.com>
2024-06-06 13:55:52 +0500

    new

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: 'UI kits',
+        value: 'ui_kits' as const,
+        featured:[
+            {
+                name: "Top Kits",
+                href: '#',
+                imageSrc: '/coffee.png'
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/1.png'
+            },
+            {
+                name: "Best Selling",
+                href: '#',
+                imageSrc: '/2.png'
+            },
+        ]
+    },
+    {
+        label: 'Icons',
+        value: 'ui_kits' as const,
+        featured:[
+            {
+                name: "Best Selling Icons",
+                href: '#',
+                imageSrc: '/3.png'
+            },
+            {
+                name: "New Arrivals",
+                href: '#',
+                imageSrc: '/4.png'
+            },
+            {
+                name: "Best Selling",
+                href: '#',
+                imageSrc: '/5.png'
+            },
+        ]
+    },
+]
