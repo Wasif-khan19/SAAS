22327913a0c318f6a52d23586070da983513595d
Wasif <wk5599426@gmail.com>
2024-06-07 21:46:07 +0500

    edited

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,9 +13,10 @@ import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
 import Link from "next/link";
+import Image from 'next/image'
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 1;
   return (
     <Sheet>
@@ -25,7 +26,7 @@ const Cart = () => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          12
+          1
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
@@ -69,7 +70,19 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div aria-hidden='true' className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image src="/cart.jpg" alt="cart" fill />
+              <div className="text-xl font-semibold">Your cart is empty</div>
+              <SheetTrigger asChild>
+                <Link href='/products' className={buttonVariants({
+                    variant: 'link',
+                    size: "sm",
+                    className: 'text-sm text-muted-foreground',
+                })}>Add to Cart for Download</Link>
+              </SheetTrigger>
+            </div>
+          </div>
         )}
       </SheetContent>
     </Sheet>
